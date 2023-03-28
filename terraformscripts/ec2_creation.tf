4b2d6d41a1655220347ae14cdf099c749b8175b2
Kishore Kunal <kunaltech508@gmail.com>
2023-03-28 07:27:05 +0000

    first commit

---
+++ terraformscripts/ec2_creation.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "AWSInstance"{
+    ami = "ami-0e07dcaca348a0e68"
+    instance_type = "t2.micro"
+    key_name = "devopsmss"
+    security_groups = ["launch-wizard-1"]
+    tags = {
+        Name = "tomcatServer"
+    }
+}
